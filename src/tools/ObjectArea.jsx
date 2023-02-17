f7980316c3fc95bf81ff44926ff03e62ec3cb50b
pl4cydo <psl2@discente.ifpe.edu.br>
2023-02-17 14:32:48 -0300

    time line com previews que rotacionam

--- src/tools/ObjectArea.jsx
+++ src/tools/ObjectArea.jsx
@@ -2,9 +2,19 @@ import '../styles/objectArea.css'
 import { useState } from 'react';
 import { arrPro, num } from '../model/Projeto';
 
+
 function ObjectArea() {
 
     const [object, setObject] = useState(arrPro[num]);
+    const [jorge, setJorge] = useState(0)
+
+    function sliderRigth() {
+        setJorge(jorge - 200)
+
+    }
+    function sliderLeft() {
+        setJorge(jorge + 200)
+    }
 
 
     return (
@@ -27,11 +37,16 @@ function ObjectArea() {
 
             <div id="TimeLine">
                 {/* <h2>Time Line</h2> */}
+                <img id='arrowSlider' src="./src/images/arrowSlider.png" alt="" onClick={() => sliderLeft()}/>
+                <img id='arrowSlider2' src="./src/images/arrowSlider.png" alt="" onClick={() => sliderRigth()}/>
+                <div style={{ 'margin-left': `${jorge}px` }} id="divid">
                     {arrPro.map((pro) =>
                         <div id="objctPreview">
 
                         </div>
                     )}
+                </div>
+                    
             </div>
 
         </div>
